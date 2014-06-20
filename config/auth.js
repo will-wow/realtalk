bab9ef21d889b100f7ff8005447d2c47326bf596
Will Lee-Wagner <william.lee.wagner@gmail.com>
2014-06-20 19:33:29 -0400

    basic connect should work

--- config/auth.js
+++ config/auth.js
@@ -1,4 +1,4 @@
-var locals = require('./config/locals');
+var locals = require('./locals');
 
 // expose our config directly to our application using module.exports
 module.exports = {
@@ -6,19 +6,19 @@ module.exports = {
   'facebookAuth': {
     'clientID'    : process.env.FACEBOOK_ID || locals.FACEBOOK_ID,
     'clientSecret': process.env.FACEBOOK_SECRET || locals.FACEBOOK_SECRET,
-    'callbackURL' : 'http://localhost:8080/auth/facebook/callback'
+    'callbackURL' : (process.env.URL || locals.URL) + '/auth/facebook/callback'
   },
 
   'twitterAuth': {
-    'consumerKey': process.env.TWITTER_ID || locals.TWITTER_ID,
+    'consumerKey'   : process.env.TWITTER_ID || locals.TWITTER_ID,
     'consumerSecret': process.env.TWITTER_SECRET || locals.TWITTER_SECRET,
-    'callbackURL': 'http://localhost:8080/auth/twitter/callback'
+    'callbackURL'   : (process.env.URL || locals.URL) + '/auth/facebook/callback'
   },
 
   'googleAuth': {
-    'clientID': process.env.GOOGLE_ID || locals.GOOGLE_ID,
+    'clientID'    : process.env.GOOGLE_ID || locals.GOOGLE_ID,
     'clientSecret': process.env.GOOGLE_SECRET || locals.GOOGLE_SECRET,
-    'callbackURL': 'http://localhost:8080/auth/google/callback'
+    'callbackURL' : (process.env.URL || locals.URL) + '/auth/facebook/callback'
   }
 
 };
